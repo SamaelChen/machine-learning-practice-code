9b96522c8c9f8dbdb2b5616231a941ea2a1bbd72
Samael <chc19910@gmail.com>
2017-03-21 14:03:21 +0800

    update

--- sp/optimization/gd.py
+++ sp/optimization/gd.py
@@ -12,14 +12,6 @@ def differnce_quotient(f, theta, i, h=0.0001):
     return (f(w) - f(theta)) / h
 
 
-# def gradient(x):
-#     return sum(2 * x_i for x_i in x)
-tmp = [differnce_quotient(cost_function, [1, 2, 3], i) for i in range(3)]
-f = cost_function
-theta = [1, 2, 3, 4]
-np.array([differnce_quotient(f, theta, j) for j in range(4)])
-
-
 def gradient_descent(f, learning_rate=0.01, tolerance=0.000001):
     theta = np.array([random.randint(-10, 10) for i in range(4)])
     # theta = [1, 2, 3, 4]
