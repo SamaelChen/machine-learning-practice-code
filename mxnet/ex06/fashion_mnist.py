9991994e805e05546c8f728cde5c0c3724316c9a
Samael <chc19910@gmail.com>
2017-08-28 17:47:45 +0800

    update

--- mxnet/ex06/fashion_mnist.py
+++ mxnet/ex06/fashion_mnist.py
@@ -50,26 +50,31 @@ train_iter, val_iter = get_mnist_iter()
 data = mx.sym.var('data')
 data = mx.sym.flatten(data=data)
 # The first fully-connected layer and the corresponding activation function
-fc1 = mx.sym.FullyConnected(data=data, num_hidden=128)
-act1 = mx.sym.Activation(data=fc1, act_type="relu")
+conv1 = mx.sym.Convolution(data=data, kernel=(5, 5), num_filter=20)
+tanh1 = mx.sym.Activation(data=conv1, act_type='tanh')
+pool1 = mx.sym.Pooling(data=tanh1, pool_type='max',
+                       kernel=(2, 2), stride=(2, 2))
 
-# The second fully-connected layer and the corresponding activation function
-fc2 = mx.sym.FullyConnected(data=act1, num_hidden=64)
-act2 = mx.sym.Activation(data=fc2, act_type="relu")
-# MNIST has 10 classes
-fc3 = mx.sym.FullyConnected(data=act2, num_hidden=10)
-# Softmax with cross entropy loss
-mlp = mx.sym.SoftmaxOutput(data=fc3, name='softmax')
+conv2 = mx.sym.Convolution(data=pool1, kernel=(5, 5), num_filter=20)
+tanh2 = mx.sym.Activation(data=conv2, act_type='tanh')
+pool2 = mx.sym.Pooling(data=tanh2, pool_type='max',
+                       kernel=(2, 2), stride=(2, 2))
+
+flatten = mx.sym.flatten(data=pool2)
+fc1 = mx.symbol.FullyConnected(data=flatten, num_hidden=500)
+tanh3 = mx.sym.Activation(data=fc1, act_type='tanh')
+fc2 = mx.symbol.FullyConnected(data=tanh3, num_hidden=10)
+lenet = mx.sym.SoftmaxOutput(data=fc2, name='softmax')
 
 import logging
 logging.getLogger().setLevel(logging.DEBUG)  # logging to stdout
-mlp_model = mx.mod.Module(symbol=mlp, context=mx.gpu(0))
-mlp_model.fit(train_iter,  # train data
-              eval_data=val_iter,  # validation data
-              optimizer='sgd',  # use SGD to train
-              # use fixed learning rate
-              optimizer_params={'learning_rate': 0.1},
-              eval_metric='acc',  # report accuracy during training
-              # output progress for each 100 data batches
-              batch_end_callback=mx.callback.Speedometer(batch_size, 100),
-              num_epoch=100)  # train for at most 10 dataset passes
+lenet_model = mx.mod.Module(symbol=lenet, context=mx.gpu(0))
+lenet_model.fit(train_iter,  # train data
+                eval_data=val_iter,  # validation data
+                optimizer='sgd',  # use SGD to train
+                # use fixed learning rate
+                optimizer_params={'learning_rate': 0.1},
+                eval_metric='acc',  # report accuracy during training
+                # output progress for each 100 data batches
+                batch_end_callback=mx.callback.Speedometer(batch_size, 100),
+                num_epoch=100)  # train for at most 10 dataset passes
