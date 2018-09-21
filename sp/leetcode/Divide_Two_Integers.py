b6afbe1bab17a7b77086cab00aa6da003ef0a015
samaelchen <chc199310@gmail.com>
2018-09-21 17:18:59 +0800

    update

--- sp/leetcode/Divide_Two_Integers.py
+++ sp/leetcode/Divide_Two_Integers.py
@@ -13,16 +13,27 @@ class Solution(object):
         :type divisor: int
         :rtype: int
         """
-        sign = (dividend < 0) is (divisor < 0)
+        neg = (dividend >= 0) ^ (divisor >= 0)
         dividend, divisor = abs(dividend), abs(divisor)
+
+        pos, base = 1, divisor
+        while base <= dividend:
+            pos <<= 1
+            base <<= 1
+
+        base >>= 1
+        pos >>= 1
+
         res = 0
-        while dividend >= divisor:
-            res += 1
-            dividend -= divisor
-        if not sign:
-            res = -res
-        return min(max(res, -2 ** 31), 2 ** 31 - 1)
+        while pos > 0:
+            if base <= dividend:
+                res += pos
+                dividend -= base
+            base >>= 1
+            pos >>= 1
+        val = -res if neg else res
+        return 2 ** 31 -1 if val > 2 ** 31 -1 else val
 
 
 s = Solution()
-s.divide(-2147, -1)
+%time s.divide(10, 3)
